0afbb8d8f07a67366cd69fe395587a6fccc88f8e
denekserhat <denekserhat@gmail.com>
2025-04-20 10:16:13 +0300

    TestController added

---
+++ src/services/CallCleaner.Api/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CallCleaner.Api.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("TEST");
+        }
+    }
+}
